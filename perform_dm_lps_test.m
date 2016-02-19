479a93098be5245b4630446a0431a227c7dffa3f
JonasNygaard <jonasnygaard@msn.com>
2016-02-19 21:57:54 +0100

    init

--- perform_dm_lps_test.m
+++ perform_dm_lps_test.m
@@ -11,10 +11,6 @@ function [DMvalue,pval] = DMtest_lps(lps1,lps2)
 diff_lps    = lps1 - lps2;
 iota        = ones(size(lps1,1),1);
 res         = nwest(diff_lps,iota,3);
-
-% [DMvalue,sbv] = olsnwhh(diff_lps,iota,3,'NW',0);
-% res_tms_stat        = nwest(ip_growth,[ones(402,1) Ltms(:,4)],3);
-
 DMvalue     = res.beta;
 pval        = 1-normcdf(res.tstat,0,1);
 
